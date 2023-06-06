b6129102eef9f7c3d57a74d285f6b15e93c45377
vrde <agranzot@mailbox.org>
2023-06-06 14:11:08 +0200

    Remove dapp

--- web/contexts/ContractsContext.ts
+++
@@ -1,7 +0,0 @@
-import { Contracts } from "@/../lib/contracts";
-
-import { createContext, useContext } from "react";
-
-export const ContractsContext = createContext<Contracts | null>(null);
-
-export const useContractsContext = () => useContext(ContractsContext);
--- web/lib/config.ts
+++
@@ -1,5 +0,0 @@
-export const TITLE = process.env.NEXT_PUBLIC_TITLE;
-export const DESCRIPTION = process.env.NEXT_PUBLIC_DESCRIPTION;
-export const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
-export const CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID);
-export const NETWORK_CONFIG = process.env.NETWORK_CONFIG;
